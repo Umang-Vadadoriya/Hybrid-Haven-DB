c71e0c919fa30d26913c4fa8d8c1921fc615be7f
krunal-BBD <160098615+krunal-BBD@users.noreply.github.com>
2024-02-18 13:57:36 +0530

    Update InsertScript.sql

--- InsertScript.sql
+++ InsertScript.sql
@@ -1,40 +1,54 @@
--- INSERT INTO Vacation (EmployeeId,VacationStartDate,VacationEndDate)
--- VALUES 
--- (5,'2023-02-15','2023-02-17'),
--- (5,'2023-02-25','2023-02-27'),
--- (6,'2023-02-15','2023-02-17'),
--- (6,'2023-02-25','2023-02-27');
+-- Insert Queries
 
--- INSERT INTO EmployeeReportsTo (EmployeeId,EmployeeReportsTo)
--- VALUES 
--- (3,1),
--- (4,1),
--- (5,2),
--- (6,2);
+-- Vacation table
 
--- INSERT INTO Events (EventName,EventDescription,EventDate)
--- VALUES 
--- ('Lunch','At Absolute Barbeque','2024-01-12 12:00:00'),
--- ('Dinner','At Absolute Barbeque','2024-01-12 12:00:00');
+INSERT INTO Vacation (EmployeeId,VacationStartDate,VacationEndDate)
+VALUES 
+(5,'2023-02-15','2023-02-17'),
+(5,'2023-02-25','2023-02-27'),
+(6,'2023-02-15','2023-02-17'),
+(6,'2023-02-25','2023-02-27');
 
--- INSERT INTO EventsEmployees (EventID,EmployeeId)
--- VALUES 
--- (1,1),
--- (1,2),
--- (1,3),
--- (1,4),
--- (1,6),
--- (2,1),
--- (2,2),
--- (2,3),
--- (2,4),
--- (2,5);
+-- EmployeeReportsTo Table
+
+INSERT INTO EmployeeReportsTo (EmployeeId,EmpReportsTo)
+VALUES 
+(3,1),
+(4,1),
+(5,2),
+(6,2);
+
+-- Events Table
+
+INSERT INTO Events (EventName,EventDescription,EventDate)
+VALUES 
+('Lunch','At Absolute Barbeque','2024-01-12 12:00:00'),
+('Dinner','At Absolute Barbeque','2024-01-12 12:00:00');
+
+-- EventsEmployees Table
+
+INSERT INTO EventsEmployees (EventID,EmployeeId)
+VALUES 
+(1,1),
+(1,2),
+(1,3),
+(1,4),
+(1,6),
+(2,1),
+(2,2),
+(2,3),
+(2,4),
+(2,5);
+
+-- NeighbourHood Table
 
 INSERT INTO NeighbourHood (NeighbourName,NeighbourNumberOfDesk)
 VALUES 
 ('Meeting',20),
 ('Hot Desk',20),
 ('Collab',32);
+
+-- DeskBooking Table
 
 INSERT INTO DeskBooking (EmployeeId,NeighbourId,DeskBookingDate)
 VALUES 
